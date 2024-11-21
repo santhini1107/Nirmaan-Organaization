0aeb53bc654c33b3783a8a56c33b802712768990
santhini1107 <132334280+santhini1107@users.noreply.github.com>
2024-11-21 12:55:41 +0530

    Add files via upload
    day2

---
+++ Task1.java
@@ -0,0 +1,21 @@
+public class Task1{
+public static void main (String[]args){
+String name1="Sasi";
+String adjective1="happy";
+String adjective2="fabulous";
+String noun1="flowers";
+String noun2="toys";
+String verb1="play";
+String noun3="piano";
+String noun4="kids";
+String adjective3="excited";
+String name2="santhini";
+String place1="mountain";
+String noun5="ice cream";
+int number=2003;
+String noun6="cows";
+
+String story= "This morning " + name1 + " woke up feeling " + adjective1 + ". 'It is going to be a " + adjective2 + " day!' Outside, a bunch of " + noun1+ "s were protesting to keep " + noun2 + " in stores. They began to " + verb1 + " to the rhythm of the " + noun3 + ", which made all the " + noun4 + "s very " + adjective3 + ". Concerned, " + name1 + " texted " + name2 + ", who flew " + name1 + " to " + place1 + " and dropped " + name1 + " in a puddle of frozen "+noun5 + ". " + name1 + " woke up in the year " + number + ", in a world where " + noun6 + "s ruled the world.";
+System.out.println(story);
+}
+}
