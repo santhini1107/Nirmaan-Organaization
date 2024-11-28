6be309695fa1c436ea27e093c4df674b13068eaf
santhini1107 <132334280+santhini1107@users.noreply.github.com>
2024-11-28 13:07:53 +0530

    StudentTask

---
+++ Student.java
@@ -0,0 +1,27 @@
+package Day5;
+
+public class Student {
+	
+		String StudentName="";
+		String DateOfBirth="";
+		int RollNum=0;
+		int Age=0;
+		public static void main(String[]args) {
+			Student Student1=new Student();
+			Student1.StudentName="Santhini";
+			Student1.DateOfBirth="04/05/2000";
+			Student1.RollNum=1023;
+			Student1.Age=24;
+		System.out.println(Student1.Age);
+		Student Student2=new Student();
+		Student2.StudentName="Sasitha";
+		Student2.DateOfBirth="04/06/2003";
+		Student2.RollNum=1024;
+		Student2.Age=21;
+	System.out.println(Student2.RollNum);
+		
+		
+		
+	}
+
+}
