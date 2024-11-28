6c24b9ab4b61da9a1417688976cf13a6429c3390
santhini1107 <132334280+santhini1107@users.noreply.github.com>
2024-11-28 11:42:48 +0530

    forLoopTask

---
+++ forLoop.java
@@ -0,0 +1,26 @@
+package day4;
+
+public class forLoop {
+	public static void main(String[]args) {
+	for(int i=1;i<=100;i++){
+//			if(i%5==0) {
+//				System.out.println("Santhini");
+//			}
+//			else {
+//				System.out.println(i);
+//			}
+		
+			if(i%2==0) {
+				System.out.println("Even");
+			}
+			else if(i%2!=0) {
+				System.out.println("odd");
+			}
+			else {
+				System.out.println("wrong");
+			}
+		}
+		
+	}
+
+}
