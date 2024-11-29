496006fb0f78c9a4a6105a10e36cf828eed7b19f
santhini1107 <132334280+santhini1107@users.noreply.github.com>
2024-11-29 13:12:39 +0530

    day6

---
+++ forTask2.java
@@ -0,0 +1,21 @@
+package day6;
+
+public class forTask2 {
+	public static void main(String[] args) {
+//		for(int i=1;i<=200;i++) {
+//			if(i==101) {
+//				break;
+//			}
+//			else if(i==34) {
+//				continue;
+//			}
+//			else {
+//				System.out.println(i);
+//			}
+//		}
+		String Name="Santhini";
+		//System.out.println(Name.toUpperCase());
+		System.out.println(Name.toLowerCase());
+	}
+
+}
