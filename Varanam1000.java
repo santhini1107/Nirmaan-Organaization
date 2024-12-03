d28fb8677f01dec9e61885e068058d1ff3a976d5
santhini1107 <132334280+santhini1107@users.noreply.github.com>
2024-12-03 12:55:47 +0530

    Day8

---
+++ Varanam1000.java
@@ -0,0 +1,15 @@
+package day8;
+
+public class Varanam1000 {
+	public static void main(String[]args) {
+		String Hero="Surya";
+		String Heroin=new String("Megana");
+		if(Hero.equals(Heroin)) {
+			System.out.println("Surya weds Megana");
+		}
+		else {
+			System.out.println("Surya weds Ramya");
+		}
+	}
+
+}
