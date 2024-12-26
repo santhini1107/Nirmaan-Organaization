08c9b98ae3404be824dd494a8e21691ada127e7e
santhini1107 <132334280+santhini1107@users.noreply.github.com>
2024-12-26 11:42:35 +0530

    day20

---
+++ ArrayListEx.java
@@ -0,0 +1,38 @@
+package day20;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class ArrayListEx {
+	public static void main(String[] args) {
+		ArrayList al=new ArrayList();
+		al.add(10);
+		al.add("Santhini");
+		al.add(10.00);
+		al.add('S');
+		System.out.println(al);
+//		ArrayList al2=new ArrayList();
+//		al2.add(50);
+//		al2.add(60);
+//		al2.add(80);
+//		al2.add(90);
+//		ArrayList al1=new ArrayList();
+//		al1.add(70);
+//		al1.add(20);
+//		al1.add(40);
+//		al1.add(30);
+//		al1.add(10);
+//		al1.addAll(al2);
+//		System.out.println("Before sort"+al1);
+//		
+//		Collections.sort(al1);
+//		System.out.println("Ascending sort"+al1);
+//		
+//		Collections.sort(al,Comparator.reverseOrder());
+//		System.out.println("Descending sort"+al1);
+		
+		
+	}
+
+}
